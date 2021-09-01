2de9f5723d8964fb8df6d9d68728391f30a7c4b7
Parminder Singh <singhparminder9411@gmail.com>
2021-09-01 16:55:51 -0700

    Add files via upload

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,8 @@
+#Parminder Singh
+#Perceptron
+
+#main driver function, inputs:threshold, adjustment, weights list, examples list, and passes wanted
+#returns nothing but prints out statements needed for the homework.
 def perceptron(threshold, adjustment, weights, examples, totalPasses):
     print("Starting weights:", weights)
     print("Threshold:",threshold, "Adjustment:",adjustment)
@@ -5,9 +10,49 @@ def perceptron(threshold, adjustment, weights, examples, totalPasses):
     for currPass in range(totalPasses): 
         print("\nPass", currPass + 1)
         for example in examples:
-            answer = example[0] # answer
-            input = example[1] # input list
+            answer = example[0] # this is the True or False part of the example
+            input = example[1] # this is the list part of the example
             print(f"inputs:{input}")
-            prediction = None
+
+            prediction = checkExamples(threshold, example, weights)
             print(f"prediction:{prediction} answer:{answer}")
-            
+
+            if (prediction != answer):
+                if (answer == True): # if should be true but is false
+                    adjustedWeightsUp = adjustUp(adjustment, input, weights)
+                    weights = adjustedWeightsUp
+                else: # if should be false but is true
+                    adjustedWeightsDown = adjustDown(adjustment, input, weights)
+                    weights = adjustedWeightsDown
+            print(f"adjusted weights:{weights}")
+    return
+
+# input: threshold, input list, weight list
+# finds the sum and returns true if greater than threshold else returns false
+def checkExamples(threshold, example, weights):
+    sum = 0
+    for i in range(len(weights)):
+        sum += (example[i]*weights[i]) 
+    if sum > threshold:
+        return True
+    else: 
+        return False   #sum is less than threshold so its false
+
+
+# input: adjustment factor, input list, weight list
+# increases weight  by the given  adjustment for every time there is a 1 
+# returns updated weights that are adjusted
+def adjustUp(adjustment, example, weights):
+    for i in range(len(example)):
+        if (example[i] == 1):
+            weights[i] += adjustment
+    return weights
+
+# input: adjustment factor, input list, weight list
+# decreases weight  by the given adjustment for every 1
+# returns updated weights that are adjusted
+def adjustDown(adjustment, example, weights):
+    for i in range(len(example)):
+        if (example[i] == 1):
+            weights[i] -= adjustment
+    return weights
